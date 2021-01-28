77e4396e62fa795a478fb38f272655ec777d03d0
GitHub Action <action@github.com>
2021-01-28 01:42:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/jiaweiwan/asmvvmtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jiaweiwan/asmvvmtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jiaweiwan.asmvvmtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jiaweiwan.asmvvmtemplate.MyBundle
 
 class MyApplicationService {
 
